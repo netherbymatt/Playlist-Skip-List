79b27bdbaa1a3ecf9e8021b81546a522e67cc193
Matthew Netherby <202042058+formalpajamas@users.noreply.github.com>
2025-11-23 14:40:55 -0700

    Uploading local files

---
+++ app/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "skiplist.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    foo().boo();
+    return 0;   
+}
---
+++ src/skiplist.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "skiplist.h"
+
+using namespace std;
+
+void foo::boo() {
+    cout << "BOO!" << endl;
+}
