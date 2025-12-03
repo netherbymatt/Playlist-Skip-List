c822d42fc468c8ca316b9e595dd021f13ad023d6
Matthew Netherby <202042058+formalpajamas@users.noreply.github.com>
2025-12-03 13:38:53 -0700

    menu enhancements

--- app/main.cpp
+++ app/main.cpp
@@ -36,9 +36,9 @@ int main() {
 
     SkipList playlist; // create skiplist upon entering program
 
-    cout << "Welcome to the Skip List Playlist - let's get started!" << endl;
+    cout << "\nWelcome to the Skip List Playlist - let's get started!" << endl;
 
-    string menu = "\nPlaylist Menu:\n1. Upload a txt file of songs\n2. Add a song to your library\n3. Remove a song from your library\n4. Search for a song in your library\n5. Create a playlist\n6. Library Stats\n7. Show Menu\n\n";
+    string menu = "\nPlaylist Menu:\n1. Upload a txt file of songs\n2. Add a song to your library\n3. Remove a song from your library\n4. Search for a song in your library\n5. Create a playlist\n6. Library Stats\n7. Show Menu\n";
 
     cout << menu;
 
@@ -47,13 +47,13 @@ int main() {
 
     while (!done) {
 
-        cout << "\nEnter the number for the operation you would like to perform (Q to quit): ";
+        cout << "\nEnter the number for the operation you would like to perform (7 to view menu, Q to quit): ";
         cin >> selection;
 
         if (cin.fail()) {done = true;} // exit program for any non-int selection
 
         else if (selection == 1) { // upload file
-            cout << "Enter the file name you would like to upload - must be a tab delimited text file (txt): ";
+            cout << "\nEnter the file name you would like to upload - must be a tab delimited text file (txt): ";
             string file_name;
             txt_verify(file_name);
             playlist.txt_input(file_name);
@@ -61,6 +61,7 @@ int main() {
         }
 
         else if (selection == 2) { // add a song to the library
+            cout << "\nLet's get the info for the song you want to add to the library." << endl;
             string artist_name;
             string song_name;
             song_input(artist_name, song_name);
@@ -70,6 +71,7 @@ int main() {
         }
 
         else if (selection == 3) { // remove a song from your library
+            cout << "\nLet's get the info for the song you want to remove from the library" << endl;
             string artist_name;
             string song_name;
             song_input(artist_name, song_name);
@@ -80,17 +82,18 @@ int main() {
         }
         
         else if (selection == 4) { // search for a sing in your library
+            cout << "\nLet's get the info for the song you want to search for in your library" << endl;
             string artist_name;
             string song_name;
             song_input(artist_name, song_name);
             string formatted_song = playlist.song_format(artist_name, song_name);
             bool song_search = playlist.search(formatted_song);
-            if (song_search == true) {cout << song_name + " by " + artist_name + " is in your library.";}
-            else {cout << song_name + " by " + artist_name + " is not in your library.";}
+            if (song_search == true) {cout << song_name << " by " << artist_name << " is in your library.\n";}
+            else {cout << song_name << " by " << artist_name << " is not in your library.\n";}
         }
 
         else if (selection == 5) {  // create a random playlist from your library
-            cout << "How many songs do you want to be in the playlist? ";
+            cout << "\nHow many songs do you want to be in the playlist? ";
             int playlist_length;
             cin >> playlist_length;
             string shuffle_playlist = playlist.shuffle(playlist_length);
@@ -102,8 +105,6 @@ int main() {
             int num_levels = playlist.get_head()->level;
             cout << "You have " << library_size << " song(s) in your library." << endl;
             cout << "This skip list has " << num_levels << " levels in it." << endl;
-            cout << "Below is a snapshot of the skip list itself: " << endl;
-            cout << playlist.list_snapshot() << endl;
         }
 
         else if (selection == 7) { // show menu
